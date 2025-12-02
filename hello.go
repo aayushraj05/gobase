7681e40c18404079ff50ef89ef1c99dd023523b2
Aayush Rajput <121253651+aayushraj05@users.noreply.github.com>
2025-12-02 16:28:52 +0530

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func aat() {
+	//code is for printing Aayush and Hello, World!
+	fmt.Println("Hello from Aayush")
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+	aat()
+}
